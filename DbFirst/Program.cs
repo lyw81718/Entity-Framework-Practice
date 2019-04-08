e8d9d1046e97b42bf6c80a49d4aaf96692c4d396
lyw81718 <44383712+lyw81718@users.noreply.github.com>
2019-04-07 22:23:44 -0500

    1

---
+++ DbFirst/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirst
+{
+    public enum Level : byte
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new PlutoDbContext();
+            var courses = context.GetCourses();
+
+            foreach (var c in courses)
+            {
+                Console.WriteLine(c.Title);
+            }
+
+
+        }
+    }
+}
